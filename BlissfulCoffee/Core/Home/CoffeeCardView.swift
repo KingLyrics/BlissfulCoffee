91347d902df3a1943dfbb824f5d3185bedc28701
aBugInTheSystem <ekom338.com@gmail.com>
2024-05-14 19:20:33 +0300

    Coffee cardview

--- BlissfulCoffee/Core/Home/CoffeeCardView.swift
+++ BlissfulCoffee/Core/Home/CoffeeCardView.swift
@@ -11,7 +11,7 @@ struct CoffeeCardView: View {
     let coffee:Coffee
     
     var body: some View {
-        VStack{
+        VStack(alignment: .leading){
             ZStack{
                 Image(coffee.image)
                     .resizable()
@@ -25,20 +25,37 @@ struct CoffeeCardView: View {
                         .scaledToFit()
                         .frame(width: 12, height: 12)
                     Text("\(String(format: "%.1f", coffee.rating))")
+                        .font(.custom("Sora-Light", size: 13))
                         .foregroundStyle(.offWhite)
                 }
                 .offset(x:39, y: -47)
             }
-            VStack(alignment:.leading){
-                Text(coffee.title)
-                    .font(.custom("Sora-Bold", size: 14))
-                Text("\(coffee.coffeType.rawValue.capitalized)")
+            VStack(alignment:.leading, spacing: 10){
+                VStack(alignment:.leading, spacing:5){
+                    Text(coffee.title)
+                        .font(.custom("Sora-Bold", size: 14))
+                    Text("\(coffee.coffeType.rawValue.capitalized)")
+                        .font(.custom("Sora-Light", size: 14))
+                        .foregroundStyle(.gray)
+                }
+                HStack(spacing:55){
+                    Text("$ \(String(format: "%.2f", coffee.price))")
+                        .font(.custom("Sora-Bold", size: 19))
+                   
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "plus")
+                            .foregroundStyle(.offWhite)
+                            .frame(width: 32, height: 32)
+                            .background(.darkBrown)
+                            .clipShape(RoundedRectangle(cornerRadius: 6))
+                    })
+                }
             }
         }
-        .padding(.vertical, 12)
-        .padding(.horizontal,10)
-        .frame(width: 160, height: 200)
-        .background(.offGray)
+        .frame(width: 180, height: 210)
+        .padding(.top, 13)
+        .padding(.bottom, 13)
+        .background(.offWhite)
         .clipShape(RoundedRectangle(cornerRadius: 12))
         
         
