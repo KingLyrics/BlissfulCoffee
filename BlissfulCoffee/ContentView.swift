7d9929a5c5bd308ae3bca31c6655517105edcea7
aBugInTheSystem <ekom338.com@gmail.com>
2024-05-14 14:46:54 +0300

    Custom font added to the application.

--- BlissfulCoffee/ContentView.swift
+++ BlissfulCoffee/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.custom("Sora-Light", size: 33))
         }
         .padding()
     }
