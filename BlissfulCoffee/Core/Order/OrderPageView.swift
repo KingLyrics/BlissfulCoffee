d990b041690c6f584e9782dcc76498bbb4494ff1
aBugInTheSystem <ekom338.com@gmail.com>
2024-05-16 09:22:39 +0300

    clean up order page.

--- BlissfulCoffee/Core/Order/OrderPageView.swift
+++ BlissfulCoffee/Core/Order/OrderPageView.swift
@@ -9,51 +9,27 @@ import SwiftUI
 
 struct OrderPageView: View {
     
-    @State private var pageView:String = "Deliver"
     
     var body: some View {
         NavigationStack{
             VStack{
-                HStack(spacing:60){
+                HStack(spacing:50){
                     Text("Deliver")
-                        .foregroundStyle(pageView == "Deliver" ? .offWhite : .black)
-                        .font(pageView == "Deliver" ? .custom("Sora-SemiBold", size: 16) : .custom("Sora-Light", size: 16))
+                        .foregroundStyle( .offWhite)
+                        .font( .custom("Sora-SemiBold", size: 16))
                         .frame(width: 153, height: 35)
-                        .background( pageView == "Deliver" ?  .darkBrown : .gray)
+                        .background(.darkBrown)
                         .clipShape(RoundedRectangle(cornerRadius: 8))
-                        .onTapGesture {
-                            pageView = "Deliver"
-                        }
                     
                     Text("Pickup")
-                       
-                    
+                        .font( .custom("Sora-Regular", size: 16))
                 }
                 .frame(width: 310, height: 43)
                 .padding(.vertical,3)
                 .background(.offGray)
                 .clipShape(RoundedRectangle(cornerRadius: 12))
                 .padding(.top)
-//
-//                Text("Deliver")
-//                    .foregroundColor(selectedOption == "Deliver" ? .white : .black)
-//                    .font(.custom("Sora-SemiBold", size: 16))
-//                    .frame(width: 153, height: 35)
-//                    .background(selectedOption == "Deliver" ? Color.brown : Color.gray)
-//                    .clipShape(RoundedRectangle(cornerRadius: 8))
-//                    .onTapGesture {
-//                        selectedOption = "Deliver"
-//                    }
-//
-//                Text("Pickup")
-//                    .foregroundColor(selectedOption == "Pickup" ? .white : .black)
-//                    .font(.custom("Sora-SemiBold", size: 16))
-//                    .frame(width: 153, height: 35)
-//                    .background(selectedOption == "Pickup" ? Color.brown : Color.gray)
-//                    .clipShape(RoundedRectangle(cornerRadius: 8))
-//                    .onTapGesture {
-//                        selectedOption = "Pickup"
-//                    }
+
                 
                 Spacer()
             }
